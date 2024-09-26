88c8d16edeb385d186e8ff1086e71a71ef3ece1d
AllhailLesha <a.goponenko0098@gmail.com>
2024-09-26 19:22:28 +0300

    ПÐКонтроллеры

---
+++ app/Http/Controllers/Base/PagesController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Base;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    private array $articles = [
+        [
+            'id' => '1',
+            'title' => 'articles title',
+            'text' => 'text'
+        ],
+        [
+            'id' => '2',
+            'title' => 'articles title 2',
+            'text' => 'articles text 2'
+        ],
+        [
+            'id' => '3',
+            'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab expedita fuga ipsa magni minus optio, possimus, quaerat, qui quia quidem quo quod recusandae voluptatem! Est eum quod saepe sit sunt!'
+        ]
+
+    ];
+
+    public function home()
+    {
+        return view('pages.home');
+    }
+
+    public function articles()
+    {
+        return view('pages.articles', [
+            'articles' => $this->articles
+        ]);
+    }
+
+    public function about()
+    {
+        return view('pages.about');
+    }
+
+    public  function showArticle($article)
+    {
+        $article = array_filter($this->articles, function ($item) use ($article){
+            return $item['id'] == $article;
+        });
+
+        $article = array_shift($article);
+
+        if (is_null($article)) {
+            return abort(404);
+        }
+
+        return view('pages.article', [
+            'title' => $article['title'] ?? 'Анонимная статья',
+            'text' => $article['text']
+        ]);
+    }
+}
